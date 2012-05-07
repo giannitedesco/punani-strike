df9566789603f6e5e5b74a31410a4d1fd1f4b929
Gianni Tedesco <gianni@scaramanga.co.uk>
2012-05-07 15:16:13 +0100

    Move game stats to dessert-stroke.h

--- dessert-stroke.h
+++ dessert-stroke.h
@@ -1,6 +1,10 @@
 #ifndef _DESSERT_STROKE_H
 #define _DESSERT_STROKE_H
 
+#define DS_STATE_LOBBY	1
+#define DS_STATE_ON	2
+#define DS_NUM_STATES	3
+
 extern const struct game_ops lobby_ops;
 extern const struct game_ops world_ops;
 
